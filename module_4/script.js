71361ce499900ba7f3348b9d707df2beb945c68c
ysgoh-999 <66617895+ysgoh-999@users.noreply.github.com>
2020-10-13 17:20:43 +0800

    Update script.js

--- module_4/script.js
+++ module_4/script.js
@@ -1,14 +1,3 @@
-// ******************************* 
-// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
-// *******************************
-//
-// Module 4 Assignment Instructions.
-//
-// The idea of this assignment is to take an existing array of names
-// and then output either Hello 'Name' or Good Bye 'Name' to the console.
-// The program should say "Hello" to any name except names that start with a "J"
-// or "j", otherwise, the program should say "Good Bye". So, the final output
-// on the console should look like this:
 /*
 Hello Yaakov
 Good Bye John
@@ -20,18 +9,9 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
-
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
-
 */
 
-// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
+
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
 
